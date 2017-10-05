097f4ccf107bd9bf5af8ca386149ad7593346449
SHREYA AGARWAL <Shreyagarwaldev@outlook.com>
2017-10-05 00:03:19 -0700

    Adding more og tags

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -14,6 +14,9 @@ export class AboutComponent {
         meta.addTags([
             { name: 'twitter:title', content: 'About Pixelated Planet' },
             { property: 'og:title', content: 'About Pixelated Planet' },
+            { property: 'og:type', content: 'article'},
+            { property: 'og:site_name', content: 'The Pixelated Planet'},
+            { property: 'fb:app_id', content: '132676104124561'},
             { property: 'og:description', content: 'PixelatedPlanet enables aspiring photographers to find workshops and seminars all around the world being conducted by people successful in the field of photography. On the other hand, we provide a platform for photographers to showcase their photography workshops and reach interested folks.' },
             { name: 'twitter:description', content: 'PixelatedPlanet enables aspiring photographers to find workshops and seminars all around the world being conducted by people successful in the field of photography. On the other hand, we provide a platform for photographers to showcase their photography workshops and reach interested folks.' },
             { property: 'og:image', content: 'https://pixelatedplanetcdn.azureedge.net/img/yosemite.jpg' },
