24c00f1da713abdab3ea62c5d7c671ff27b650b3
SHREYA AGARWAL <Shreyagarwaldev@outlook.com>
2017-10-04 23:52:31 -0700

    fixing meta tags'

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -12,14 +12,14 @@ export class AboutComponent {
         gaService.trackPageView('About');
         title.setTitle('About Pixelated Planet, the photographers and their workshop listings')
         meta.addTags([
-            { property: 'twitter:title', content: 'About Pixelated Planet, the photographers and their workshop listings' },
-            { property: 'og:title', content: 'About Pixelated Planet, the photographers and their workshop listings' },
+            { name: 'twitter:title', content: 'About Pixelated Planet' },
+            { property: 'og:title', content: 'About Pixelated Planet' },
             { property: 'og:description', content: 'PixelatedPlanet enables aspiring photographers to find workshops and seminars all around the world being conducted by people successful in the field of photography. On the other hand, we provide a platform for photographers to showcase their photography workshops and reach interested folks.' },
-            { property: 'twitter:description', content: 'PixelatedPlanet enables aspiring photographers to find workshops and seminars all around the world being conducted by people successful in the field of photography. On the other hand, we provide a platform for photographers to showcase their photography workshops and reach interested folks.' },
+            { name: 'twitter:description', content: 'PixelatedPlanet enables aspiring photographers to find workshops and seminars all around the world being conducted by people successful in the field of photography. On the other hand, we provide a platform for photographers to showcase their photography workshops and reach interested folks.' },
             { property: 'og:image', content: 'https://pixelatedplanetcdn.azureedge.net/img/yosemite.jpg' },
-            { property: 'twitter:image', content: 'https://pixelatedplanetcdn.azureedge.net/img/yosemite.jpg' },
+            { name: 'twitter:image', content: 'https://pixelatedplanetcdn.azureedge.net/img/yosemite.jpg' },
             { property: 'og:url', content: 'https://www.thepixelatedplanet.com/about' },
-            { property: 'twitter:site', content: 'https://www.thepixelatedplanet.com/about' },
+            { name: 'twitter:site', content: 'https://www.thepixelatedplanet.com/about' },
           ]);
     }
 }
