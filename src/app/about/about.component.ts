dc48458af582b54db4e400fb1a599f888af500fd
SHREYA AGARWAL <Shreyagarwaldev@outlook.com>
2017-10-04 23:41:26 -0700

    meta and title for about'

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser'
 import { GoogleAnalyticsService } from '../services/analytics/googleAnalyticsService'
 
 @Component({
@@ -7,7 +8,18 @@ import { GoogleAnalyticsService } from '../services/analytics/googleAnalyticsSer
 })
 
 export class AboutComponent {
-    constructor(gaService : GoogleAnalyticsService) {
+    constructor(gaService : GoogleAnalyticsService, title: Title, meta: Meta) {
         gaService.trackPageView('About');
+        title.setTitle('About Pixelated Planet, the photographers and their workshop listings')
+        meta.addTags([
+            { property: 'twitter:title', content: 'About Pixelated Planet, the photographers and their workshop listings' },
+            { property: 'og:title', content: 'About Pixelated Planet, the photographers and their workshop listings' },
+            { property: 'og:description', content: 'PixelatedPlanet enables aspiring photographers to find workshops and seminars all around the world being conducted by people successful in the field of photography. On the other hand, we provide a platform for photographers to showcase their photography workshops and reach interested folks.' },
+            { property: 'twitter:description', content: 'PixelatedPlanet enables aspiring photographers to find workshops and seminars all around the world being conducted by people successful in the field of photography. On the other hand, we provide a platform for photographers to showcase their photography workshops and reach interested folks.' },
+            { property: 'og:image', content: 'https://pixelatedplanetcdn.azureedge.net/img/yosemite.jpg' },
+            { property: 'twitter:image', content: 'https://pixelatedplanetcdn.azureedge.net/img/yosemite.jpg' },
+            { property: 'og:url', content: 'https://www.thepixelatedplanet.com/about' },
+            { property: 'twitter:site', content: 'https://www.thepixelatedplanet.com/about' },
+          ]);
     }
 }
