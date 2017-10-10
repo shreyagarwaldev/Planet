3777cb41f99c53149d5c08b3e02098edacc1ae39
SHREYA AGARWAL <Shreyagarwaldev@outlook.com>
2017-10-09 23:03:56 -0700

    trymeta

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -22,20 +22,20 @@ export class HomeComponent implements OnInit {
     this.globalConstants = globalConstantsRepository;
     this.gaService.trackPageView('Home');
     title.setTitle('Travel, Wildlife, Portrait, Street, etc. Photography Workshops')
-    meta.addTags([
-        { name: 'twitter:title', content: 'Photography Workshops - Pixelated Planet' },
-        { property: 'og:title', content: 'Photography Workshops - Pixelated Planet' },
-        { property: 'og:type', content: 'website'},
-        { property: 'og:site_name', content: 'The Pixelated Planet'},
-        { property: 'fb:app_id', content: '132676104124561'},
-        { name: 'description', content: `Enabling aspiring photographers to find workshops in travel, nature, wildlife, street, portrait, etc photography while providing a platform for photographers' workshops` },
-        { property: 'og:description', content: `Enabling aspiring photographers to find workshops in travel, nature, wildlife, street, portrait, etc photography while providing a platform for photographers' workshops` },
-        { name: 'twitter:description', content: `Enabling aspiring photographers to find workshops in travel, nature, wildlife, street, portrait, etc photography while providing a platform for photographers' workshops` },
-        { property: 'og:image', content: 'http://www.thepixelatedplanet.com/assets/img/yosemite.jpg' },
-        { name: 'twitter:image', content: 'http://www.thepixelatedplanet.com/assets/img/yosemite.jpg' },
-        { property: 'og:url', content: 'https://www.thepixelatedplanet.com/' },
-        { name: 'twitter:site', content: 'https://www.thepixelatedplanet.com/' },
-      ]);
+    // meta.addTags([
+    //     { name: 'twitter:title', content: 'Photography Workshops - Pixelated Planet' },
+    //     { property: 'og:title', content: 'Photography Workshops - Pixelated Planet' },
+    //     { property: 'og:type', content: 'website'},
+    //     { property: 'og:site_name', content: 'The Pixelated Planet'},
+    //     { property: 'fb:app_id', content: '132676104124561'},
+    //     { name: 'description', content: `Enabling aspiring photographers to find workshops in travel, nature, wildlife, street, portrait, etc photography while providing a platform for photographers' workshops` },
+    //     { property: 'og:description', content: `Enabling aspiring photographers to find workshops in travel, nature, wildlife, street, portrait, etc photography while providing a platform for photographers' workshops` },
+    //     { name: 'twitter:description', content: `Enabling aspiring photographers to find workshops in travel, nature, wildlife, street, portrait, etc photography while providing a platform for photographers' workshops` },
+    //     { property: 'og:image', content: 'http://www.thepixelatedplanet.com/assets/img/yosemite.jpg' },
+    //     { name: 'twitter:image', content: 'http://www.thepixelatedplanet.com/assets/img/yosemite.jpg' },
+    //     { property: 'og:url', content: 'https://www.thepixelatedplanet.com/' },
+    //     { name: 'twitter:site', content: 'https://www.thepixelatedplanet.com/' },
+    //   ]);
   }
 
   ngOnDestroy() {
